fab8f1e615e4cd6162b9ddc7ee8ed42ee111485a
josejmv <josejmvasquez@gmail.com>
2021-12-27 03:31:39 -0400

    [hotfix] delete example testings

--- next.config.js
+++ next.config.js
@@ -1,5 +1,7 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-  env:{ NEXTAUTH_URL: process.env.NEXTAUTH_URL }
+  env: {
+    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+  },
 }
