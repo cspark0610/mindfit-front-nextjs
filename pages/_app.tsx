44ab2e35dba82b58cbd512d6e5da574e662cc27c
Anthony <anthony560@hotmail.com>
2021-12-28 20:54:41 -0400

    [FEATURE] add imports of primeReact and primeIcons

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,7 @@
 import 'styles/theme.scss'
+import 'primereact/resources/primereact.min.css'
+import 'primeicons/primeicons.css'
+import 'primereact/resources/themes/lara-light-indigo/theme.css'
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
