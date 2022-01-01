07eb68cbfeb99ec1aa34de4590fae97fc1cc9dfb
Anthony <anthony560@hotmail.com>
2022-01-01 18:09:49 -0400

    [FEATURE] Create CoachContentModal

---
+++ src/components/molecules/CoachContentModal/index.tsx
@@ -0,0 +1,16 @@
+// Main tools
+
+// Components
+
+// Styles
+import { Container } from 'react-bootstrap'
+
+// Types
+
+export const CoachContentModal = () => {
+  return (
+    <Container>
+      <h1>Soy el contenido que vendra para los coach</h1>
+    </Container>
+  )
+}
