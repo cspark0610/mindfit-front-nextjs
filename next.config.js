5ce7ae40bcbc73ba610b61d858847b5ce20074ef
josejmv <josejmvasquez@gmail.com>
2022-01-07 11:20:31 -0400

    [hotfix] fix next config validation for env variables

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ module.exports = {
   reactStrictMode: true,
   env: {
     NEXTAUTH_URL:
-      process.env.NODE_ENV === 'development'
+      process.env.NODE_ENV !== 'production'
         ? 'http://localhost:3000'
         : process.env.NEXTAUTH_URL,
     SECRET: process.env.SECRET,
