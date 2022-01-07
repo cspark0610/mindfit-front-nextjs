26e0657518c1baf55264a8233fbf8a9ad756d01c
josejmv <josejmvasquez@gmail.com>
2022-01-06 20:42:17 -0400

    [hotfix] change form action attribute in next auth login for cypress testing

--- cypress/support/Auth/commands.ts
+++ cypress/support/Auth/commands.ts
@@ -4,5 +4,16 @@ Cypress.Commands.add('loginWithCredentials', () => {
 
   cy.get('input[name=email]').type('centriadevelopment@gmail.com')
   cy.get('input[name=password]').type('123qwe!@#')
+  cy.get('form')
+    .invoke(
+      'attr',
+      'action',
+      'http://localhost:3000/api/auth/callback/credentials'
+    )
+    .should(
+      'have.attr',
+      'action',
+      'http://localhost:3000/api/auth/callback/credentials'
+    )
   cy.get('button[type=submit]').click()
 })
