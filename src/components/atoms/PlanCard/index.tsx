bcb6084742f9c2b9179a5264498d175c134783b5
Anthony <anthony560@hotmail.com>
2022-01-11 13:08:08 -0400

    [FEATURE] add payment method modal

--- src/components/atoms/PlanCard/index.tsx
+++ src/components/atoms/PlanCard/index.tsx
@@ -1,5 +1,11 @@
+// Main utils
+import { useState } from 'react'
+
+// Components
+import { PaymentMethodCard } from 'components/molecules/PaymentMethodCard'
+
 // bootstrap components
-import { Container, Row, Col, Button } from 'react-bootstrap'
+import { Container, Row, Col, Button, Modal } from 'react-bootstrap'
 import { CheckCircleFill, XCircleFill } from 'react-bootstrap-icons'
 
 // styles
@@ -8,27 +14,46 @@ import classes from 'styles/Choose-plan/page.module.scss'
 // types
 import { FC } from 'react'
 
-export const PlanCard: FC<{ selected?: boolean }> = ({ selected }) => (
-  <Container className={selected ? classes.banner_selected : classes.banner}>
-    <div className={classes.card}>
-      <h4 className={classes.card_title}>Demo</h4>
-      <h5 className={classes.card_price}>120$</h5>
-      <Row className={classes.card_items}>
-        <Col xs={12}>
-          <CheckCircleFill className={classes.card_items_include} /> Lorem ipsum
-        </Col>
-        <Col xs={12}>
-          <CheckCircleFill className={classes.card_items_include} /> Lorem ipsum
-        </Col>
-        <Col xs={12}>
-          <XCircleFill className={classes.card_items_noinclude} /> Lorem ipsum
-        </Col>
-      </Row>
-      <Row className='mt-5'>
-        <Col xs={12}>
-          <Button className={classes.button_small}>Adquirir</Button>
-        </Col>
-      </Row>
-    </div>
-  </Container>
-)
+export const PlanCard: FC<{ selected?: boolean }> = ({ selected }) => {
+  const [showModal, setShowModal] = useState(false)
+  const handleCloseModal = () => setShowModal(false)
+  const handleShowModal = () => setShowModal(true)
+  return (
+    <Container className={selected ? classes.banner_selected : classes.banner}>
+      <div className={classes.card}>
+        <h4 className={classes.card_title}>Demo</h4>
+        <h5 className={classes.card_price}>120$</h5>
+        <Row className={classes.card_items}>
+          <Col xs={12}>
+            <CheckCircleFill className={classes.card_items_include} /> Lorem
+            ipsum
+          </Col>
+          <Col xs={12}>
+            <CheckCircleFill className={classes.card_items_include} /> Lorem
+            ipsum
+          </Col>
+          <Col xs={12}>
+            <XCircleFill className={classes.card_items_noinclude} /> Lorem ipsum
+          </Col>
+        </Row>
+        <Row className='mt-5'>
+          <Col xs={12}>
+            <Button className={classes.button_small} onClick={handleShowModal}>
+              Adquirir
+            </Button>
+          </Col>
+        </Row>
+      </div>
+      <Modal
+        centered
+        className={classes.modal}
+        show={showModal}
+        onHide={handleCloseModal}
+        size='lg'>
+        <Modal.Body>
+          <PaymentMethodCard handleCloseModal={handleCloseModal} />
+        </Modal.Body>
+      </Modal>
+    </Container>
+  )
+}
