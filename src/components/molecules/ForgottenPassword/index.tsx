a9603ea782ed56862173dfba615184ce2fa965d8
Ubaldo <sarcosas.dev@gmail.com>
2022-01-18 11:53:38 -0400

    [feature]e9a293-reset-password integrated requestResetPassword mutation

--- src/components/molecules/ForgottenPassword/index.tsx
+++ src/components/molecules/ForgottenPassword/index.tsx
@@ -12,6 +12,9 @@ import classes from 'styles/Login/ForgottenPassword/forgottenPassword.module.scs
 // Types
 import { ChangeType, SetStateType } from 'types'
 import { FC } from 'react'
+import { useMutation } from '@apollo/client'
+import RESET_PASSWORD from 'lib/mutations/resetPassword.gql'
+import { validateEmail } from 'utils/validateEmail'
 
 interface Props {
   setToggleView: SetStateType<boolean>
@@ -20,19 +23,45 @@ interface Props {
 
 export const ForgottenPassword: FC<Props> = ({ setToggleView, content }) => {
   const [userEmail, setUserEmail] = useState('')
+  const [showError, setShowError] = useState(false)
+  const [requestedPassword, setRequestedPassword] = useState(false)
 
   const handleToggleChange = () => {
     setToggleView((currentValue) => !currentValue)
   }
 
   const handleChange = (ev: ChangeType) => {
-    setUserEmail(ev.target.value)
+    setUserEmail(ev.target.value.trim())
+  }
+
+  const [requestResetPassword] = useMutation(RESET_PASSWORD, {
+    onCompleted: ({ requestedResetPassword }) => {
+      console.log('holi', requestedResetPassword)
+      setShowError(false)
+      setRequestedPassword(requestedResetPassword)
+    },
+    onError: () => {
+      setShowError(true)
+      setRequestedPassword(false)
+    },
+  })
+
+  const handleSubmit = (e: any) => {
+    e.preventDefault()
+    try {
+      if (validateEmail(userEmail))
+        requestResetPassword({ variables: { email: userEmail } })
+    } catch (error) {
+      console.log(error)
+    }
   }
 
   return (
     <Row className={classes.container}>
       <Col xs={12} className='d-flex justify-content-center'>
-        <form className={`${classes.card} ${classes.section}`}>
+        <form
+          onSubmit={handleSubmit}
+          className={`${classes.card} ${classes.section}`}>
           <Row>
             <InputText
               className={`${classes.marginInput} ${classes.input}`}
@@ -40,9 +69,14 @@ export const ForgottenPassword: FC<Props> = ({ setToggleView, content }) => {
               onChange={handleChange}
               placeholder={content.email.placeholder}
             />
+            {showError && (
+              <span className={classes.errorText}>
+                Error: No se encuentra el usuario
+              </span>
+            )}
           </Row>
           <Row>
-            <Button className={`my-5 ${classes.button}`}>
+            <Button type='submit' className={`my-5 ${classes.button}`}>
               {content.sendEmailButton}
             </Button>
           </Row>
