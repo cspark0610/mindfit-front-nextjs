f977a356718ecc5e6f1d986143cccc77e893596c
Anthony <anthony560@hotmail.com>
2022-01-20 17:21:36 -0400

    [HOTFIX] Refactor in valideCompanySignup

--- src/components/organisms/OrganizationSignup/Company/utils.ts
+++ src/components/organisms/OrganizationSignup/Company/utils.ts
@@ -2,7 +2,7 @@
 import { CompanyDataType } from 'types/models/Company'
 
 export const validateCompanySignup = (companyData: CompanyDataType) => {
-  if (!companyData.name || !companyData.description || !companyData.picture)
+  if (!companyData.name || !companyData.about || !companyData.profilePicture)
     return false
 
   return true
