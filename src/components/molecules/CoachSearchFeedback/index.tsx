173853be4b9207a9477e442cbb1857bb029acdb3
Anthony <anthony560@hotmail.com>
2022-01-25 16:00:27 -0400

    [FEATURE] Refactor in checkBox and add strapi

--- src/components/molecules/CoachSearchFeedback/index.tsx
+++ src/components/molecules/CoachSearchFeedback/index.tsx
@@ -21,14 +21,19 @@ import { CheckboxChangeParams } from 'primereact/checkbox'
 interface CoachFeedbackInterface {
   cancel: () => void
   submit: () => void
+  content: any
 }
 
 export const CoachSearchFeedback: FC<CoachFeedbackInterface> = ({
+  content,
   cancel,
   submit,
 }) => {
   const [option, setOption] = useState('')
   const [details, setDetails] = useState('')
+  // const checkboxOptions = ['firstOption', 'secondOption', 'other']
+
+  console.log(content)
 
   const checkStylesValidation = (value: string) =>
     option === value ? classes.check : classes.uncheck
@@ -44,62 +49,45 @@ export const CoachSearchFeedback: FC<CoachFeedbackInterface> = ({
 
   return (
     <div className={classes.section}>
-      <h2 className={classes.title}>
-        ¿No estás satisfecho con los coach sugeridos?
-      </h2>
+      <h2 className={classes.title}>{content.suggestedCoachesTitle}</h2>
       <Container>
         <form onSubmit={handleSubmit}>
-          <p className={classes.description}>Puedes indicarnos ¿por qué no?</p>
-          <div className={classes.options}>
-            <label className={checkStylesValidation('firstOption')}>
-              <Checkbox
-                className='me-4'
-                checked={option === 'firstOption'}
-                onChange={handleOptionChange}
-                value='firstOption'
-              />
-              No estoy identificado con los coach mostrados.
-            </label>
+          <p className={classes.description}>
+            {content.suggestedCoachesDescription}
+          </p>
+          <div className={`${classes.options} d-flex flex-column`}>
+            {content.suggestedCoachesOptions.map((options: any) => (
+              <label
+                key={options.id}
+                className={`${checkStylesValidation(options.value)} mb-3`}>
+                <Checkbox
+                  className='me-4'
+                  checked={option === options.value}
+                  onChange={handleOptionChange}
+                  value={options}
+                />
+                {options.label}
+              </label>
+            ))}
           </div>
-          <div className={classes.options}>
-            <label className={checkStylesValidation('secondOption')}>
-              <Checkbox
-                className='me-4'
-                checked={option === 'secondOption'}
-                onChange={handleOptionChange}
-                value='secondOption'
-              />
-              Ninguno aborda mi debilidad a trabajar.
-            </label>
-          </div>
-          <div className={classes.options}>
-            <label className={checkStylesValidation('other')}>
-              <Checkbox
-                className='me-4'
-                checked={option === 'other'}
-                onChange={handleOptionChange}
-                value={'other'}
-              />
-              Otros
-            </label>
-          </div>
-          <p>Explique su respuesta</p>
+          <p>{content.suggestedCoachesTextarea.label}</p>
           <InputTextarea
             autoResize
             rows={8}
+            placeholder={content.suggestedCoachesTextarea.placeholder}
             value={details}
             onChange={(ev) => setDetails(ev.target.value)}
             disabled={option !== 'other'}
             className={classes.textarea}
           />
           <Button type='submit' className={`${classes.button} mt-4`}>
-            Enviar
+            {content.suggestedCoachesSubmitButton.label}
           </Button>
           <Button
             variant='link'
             className={classes.buttonLink}
             onClick={cancel}>
-            Cancelar
+            {content.suggestedCoachesCancelButton.label}
           </Button>
         </form>
       </Container>
