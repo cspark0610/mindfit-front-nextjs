be991e3b89c0e8e7e0de5e6afb948e0194b733df
Anthony <anthony560@hotmail.com>
2022-01-27 11:37:31 -0400

    [FEATURE] example view

---
+++ pages/templates/index.tsx
@@ -0,0 +1,19 @@
+import {
+  Cover,
+  Greeting,
+  ProfileTypes,
+  Farewell,
+} from 'components/molecules/SatResults'
+
+const Templates = () => {
+  return (
+    <div>
+      <Cover />
+      <Greeting />
+      <ProfileTypes />
+      <Farewell />
+    </div>
+  )
+}
+
+export default Templates
