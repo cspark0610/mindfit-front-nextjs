a6aff1a9c057f133c24de5b390688db7934443df
Anthony <anthony560@hotmail.com>
2022-02-03 14:19:58 -0400

    [FEATURE] SAT carousel

--- pages/templates/index.tsx
+++ pages/templates/index.tsx
@@ -1,22 +1,46 @@
-import {
-  Cover,
-  Greeting,
-  Farewell,
-  Graph,
-  Percentages,
-  ProfileTypes,
-} from 'components/molecules/SatResults'
+// Component
+import { SAT_TEMPLATES } from 'components/molecules/SatResults'
+import { Layout } from 'components/organisms/Layout'
+import { Container } from 'react-bootstrap'
+
+// Boostrap Component
+import Carousel from 'react-bootstrap/Carousel'
+
+// Styles
+import classes from 'styles/SatTemplates/satTemplates.module.scss'
 
 const Template = () => {
   return (
-    <div>
-      <Cover />
-      <Greeting />
-      <ProfileTypes />
-      <Graph />
-      <Percentages />
-      <Farewell />
-    </div>
+    <Layout>
+      <Container fluid className={classes.container}>
+        <Carousel className={classes.carouselShadow}>
+          {/* <Carousel.Item>{SAT_TEMPLATES['cover']()}</Carousel.Item> */}
+          <Carousel.Item className={` ${classes.gradient} ${classes.bg}`}>
+            <SAT_TEMPLATES.cover />
+          </Carousel.Item>
+
+          <Carousel.Item className={classes.bg}>
+            <SAT_TEMPLATES.greeting />
+          </Carousel.Item>
+
+          <Carousel.Item className={classes.bg}>
+            <SAT_TEMPLATES.profileTypes />
+          </Carousel.Item>
+
+          <Carousel.Item className={classes.bg}>
+            <SAT_TEMPLATES.graphics />
+          </Carousel.Item>
+
+          <Carousel.Item className={classes.bg}>
+            <SAT_TEMPLATES.percentages />
+          </Carousel.Item>
+
+          <Carousel.Item className={classes.bg}>
+            <SAT_TEMPLATES.farewell />
+          </Carousel.Item>
+        </Carousel>
+      </Container>
+    </Layout>
   )
 }
 
