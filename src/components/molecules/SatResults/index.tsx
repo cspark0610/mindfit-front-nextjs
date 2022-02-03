7f1092d995583941de3dea67dd5d19e8c42731e1
Anthony <anthony560@hotmail.com>
2022-02-03 14:20:23 -0400

    [FEATURE] SAT_TEMPLATES

--- src/components/molecules/SatResults/index.tsx
+++ src/components/molecules/SatResults/index.tsx
@@ -1,6 +1,15 @@
-export { Cover } from './Cover'
-export { Greeting } from './Greeting'
-export { ProfileTypes } from './ProfileTypes'
-export { Farewell } from './Farewell'
-export { Percentages } from './Percentages'
-export { Graph } from './Graphics'
+import { Cover } from './Cover'
+import { Greeting } from './Greeting'
+import { Percentages } from './Percentages'
+import { ProfileTypes } from './ProfileTypes'
+import { Graph } from './Graphics'
+import { Farewell } from './Farewell'
+
+export const SAT_TEMPLATES = {
+  cover: () => <Cover />,
+  greeting: (props: any) => <Greeting {...props} />,
+  percentages: (props: any) => <Percentages {...props} />,
+  profileTypes: (props: any) => <ProfileTypes {...props} />,
+  graphics: (props: any) => <Graph {...props} />,
+  farewell: (props: any) => <Farewell {...props} />,
+}
