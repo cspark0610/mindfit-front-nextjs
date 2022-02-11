09351cbb2d993144f00174f9a77e3d5159d8e79c
Anthony <anthony560@hotmail.com>
2022-02-11 09:27:48 -0400

    [FEATURE] refactore and connect with strapi

--- src/components/molecules/SatResults/Cover/index.tsx
+++ src/components/molecules/SatResults/Cover/index.tsx
@@ -1,6 +1,9 @@
 // Animation
-import { motion } from 'framer-motion'
 import { fadeIn } from 'commons/animations'
+import { CardMotion } from 'components/atoms/AnimateComponents'
+
+// Bootstrap Component
+import { Card } from 'react-bootstrap'
 
 // Styles
 import classes from 'styles/Cover/cover.module.scss'
@@ -8,15 +11,21 @@ import classes from 'styles/Cover/cover.module.scss'
 // Type
 import { FC } from 'react'
 
-export const Cover: FC = () => {
+export const Cover: FC<any> = (props) => {
   return (
-    <motion.div {...fadeIn} className={classes.container}>
-      <h1 className={classes.title}>
-        Informe de <br /> <b>pruebas diagnósticas</b>
-      </h1>
-      <p className={classes.copyright}>
-        Haki Health @ Todos los derechos reservados
-      </p>
-    </motion.div>
+    <CardMotion {...fadeIn} className={`${classes.container} ${classes.bg}`}>
+      <Card.Img
+        className='opacity-25'
+        src={props.background.data.attributes.url}
+      />
+      <Card.ImgOverlay className={classes.bg}>
+        <h1
+          className={classes.title}
+          dangerouslySetInnerHTML={{ __html: props.title }}
+        />
+
+        <p className={classes.copyright}>{props.copyright}</p>
+      </Card.ImgOverlay>
+    </CardMotion>
   )
 }
--- src/components/molecules/SatResults/Percentages/index.tsx
+++ src/components/molecules/SatResults/Percentages/index.tsx
@@ -17,15 +17,24 @@ import classes from 'styles/Percentages/percentages.module.scss'
 // types
 import { FC } from 'react'
 
-export const Percentages: FC = () => {
+export const Percentages: FC<any> = (props) => {
+  const qualificationData = props.qualificationData.data
+
+  const comunicationData = props.satResults.result.find(
+    (result: any) => result.areaCodeName === 'SUBORDINATE'
+  )
+  const getQualification = (section: any, title: string) => {
+    return section.puntuations.find(
+      (puntuation: any) => puntuation.name === title
+    )
+  }
+
   const overlayTooltip = () => <Tooltip>Loren ipsum</Tooltip>
   return (
     <ContainerMotion fluid {...viewportFadeIn} className={classes.container}>
       <Row>
-        <p className={classes.header}>
-          Ahora es el turno de los resultados referentes a la empresa
-        </p>
-        <h1 className={classes.title}>Comunicación en la empresa</h1>
+        <p className={classes.header}>{props.subtitle}</p>
+        <h1 className={classes.title}>{props.title}</h1>
       </Row>
       <Row>
         <Col lg={4}>
@@ -38,48 +47,44 @@ export const Percentages: FC = () => {
           />
           <p>
             <Send className={classes.icon} />
-            Descubre cómo se comunican los miembros de tu empresa
+            {props.anchorText}
           </p>
         </Col>
         <Col lg={3}>
-          {[0, 1, 2].map((item) => (
-            <OverlayTrigger
-              key={item}
-              placement='bottom'
-              overlay={overlayTooltip()}>
-              <div className={classes.cardPoint}>
-                <h4>
-                  <span className={classes.point}>4,3</span> / 5
-                </h4>
-                <div>Comunicación ascendente</div>
-              </div>
-            </OverlayTrigger>
-          ))}
+          {qualificationData.map((data: any, idx: number) => {
+            const area = getQualification(
+              comunicationData,
+              data.attributes.category
+            )
+            return (
+              <OverlayTrigger
+                key={idx}
+                placement='bottom'
+                overlay={overlayTooltip()}>
+                <div className={classes.cardPoint}>
+                  <h4>
+                    <span className={classes.point}>{area.value}</span> /{' '}
+                    {area.base}
+                  </h4>
+                  <div>{data.attributes.title}</div>
+                </div>
+              </OverlayTrigger>
+            )
+          })}
         </Col>
         <Col lg={5}>
-          <p className={classes.subtitle}>¿Qué significan estos factores?</p>
-          <p>
-            Los resultados obtenidos en estos tres factores, se encuentran
-            destacadamente por encima de la media deseable en entornos
-            corporativos, estando la comunicación horizontal ligeramente por
-            debajo de la ascendente o descendente.
-          </p>
-          <p>
-            En las empresas, los efectos positivos de la comunicación son
-            evidentes: mejora la competitividad de la organización, así como la
-            forma en la que se puede adaptar a los cambios que se produzcan en
-            su entorno, con el fin de conseguir los objetivos que se hayan
-            propuesto inicialmente. Al mismo tiempo, la existencia de una
-            comunicación en la empresa eficaz, fomenta la motivación de los
-            empleados, así como el compromiso y la implicación en las tareas
-            corporativas, creando un clima de trabajo integrador.
-          </p>
-          <p>
-            Por ello, a pesar de encontrarnos en niveles por encima de la media
-            deseable, todo esfuerzo por mejorar dicha comunicación corporativa,
-            incidirá en la mejora en todos esos efectos.
-          </p>
-          <Button className={classes.button}>Adceder a los recursos</Button>
+          <ul>
+            {qualificationData.map(({ attributes }: any, idx: number) => (
+              <li key={idx}>
+                <p>{attributes.title}</p>
+                <div
+                  dangerouslySetInnerHTML={{ __html: attributes.description }}
+                />
+              </li>
+            ))}
+          </ul>
+
+          <Button className={classes.button}>Acceder a los recursos</Button>
         </Col>
       </Row>
     </ContainerMotion>
