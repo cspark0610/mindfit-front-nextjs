481b37895516b08af7b5293b7950d412f052a487
Anthony <anthony560@hotmail.com>
2022-02-11 09:29:02 -0400

    [FEATURE] add SAT_TEMPLATES

--- src/components/molecules/SatResults/index.tsx
+++ src/components/molecules/SatResults/index.tsx
@@ -1,3 +1,4 @@
+import { forwardRef } from 'react'
 import { Cover } from './Cover'
 import { Greeting } from './Greeting'
 import { Percentages } from './Percentages'
@@ -5,11 +6,19 @@ import { ProfileTypes } from './ProfileTypes'
 import { Graph } from './Graphics'
 import { Farewell } from './Farewell'
 
+// const SatReportIntro = forwardRef((props: any) => {
+//   return <p {...props} />
+// })
+// const SatReportGreeting = forwardRef((props: any, ref) => <Percentages ref={ref} {...props} />)
+// const SatReportQualification = forwardRef((props: any, ref) => <Cover ref={ref} {...props} />)
+// const SatReportIntro = forwardRef((props: any, ref) => <Cover ref={ref} {...props} />)
+// const SatReportIntro = forwardRef((props: any, ref) => <Cover ref={ref} {...props} />)
+
 export const SAT_TEMPLATES = {
-  cover: () => <Cover />,
-  greeting: (props: any) => <Greeting {...props} />,
-  percentages: (props: any) => <Percentages {...props} />,
-  profileTypes: (props: any) => <ProfileTypes {...props} />,
-  graphics: (props: any) => <Graph {...props} />,
-  farewell: (props: any) => <Farewell {...props} />,
+  SatReportIntro: (props: any) => <Cover {...props} />,
+  SatReportGreeting: (props: any) => <Greeting {...props} />,
+  SatReportQualification: (props: any) => <Percentages {...props} />,
+  SatReportInformation: (props: any) => <ProfileTypes {...props} />,
+  SatReportGraph: (props: any) => <Graph {...props} />,
+  SatReportFarewell: (props: any) => <Farewell {...props} />,
 }
