a36b250fb86d6108639ad3b1dced93ae6cc26700
sindi <sindidaconceicao@gmail.com>
2022-02-21 08:54:32 -0400

    [feature][3fefe0] library article page getStaticPaths

--- pages/library/index.tsx
+++ pages/library/index.tsx
@@ -1,5 +1,6 @@
 // main tools
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
+import { useRouter } from 'next/router'
 
 // bootstrap components
 import { Container, Row, Col, Spinner } from 'react-bootstrap'
@@ -12,8 +13,8 @@ import { Filter } from 'components/organisms/Library/filter'
 
 // gql
 import { useQuery } from '@apollo/client'
+import { initializeApolloClient } from 'lib/apollo'
 import GET_LISTS_OF_POSTS from 'lib/strapi/queries/Library/getListsOfPosts.gql'
-import POST_CATEGORIES from 'lib/strapi/queries/Library/postCategories.gql'
 
 // commons
 import { microServices } from 'commons'
@@ -22,32 +23,24 @@ import { microServices } from 'commons'
 import classes from 'styles/Library/page.module.scss'
 
 // types
-import { NextPage } from 'next'
+import { GetServerSidePropsContext, NextPage } from 'next'
+import { GetSSPropsType } from 'types'
 
-const LibraryPage: NextPage = () => {
-  const [categories, setCategories] = useState([])
+const LibraryPage: NextPage<GetSSPropsType<typeof getServerSideProps>> = ({
+  defaultCategory,
+  postsCategories,
+}) => {
   const [posts, setPosts] = useState([])
+  const { locale } = useRouter()
 
   const { loading, refetch } = useQuery(GET_LISTS_OF_POSTS, {
     context: { ms: microServices.strapi },
     variables: {
-      locale: 'es',
+      locale: locale,
       filters: {},
     },
     onCompleted: (data) => {
       setPosts(data.posts.data)
-      const postsCategories = data.posts.data.map((post: any) =>
-        post.attributes.postCategories.data.map(
-          (category: any) => category.attributes.category
-        )
-      )
-      if (!categories.length) {
-        const inputCategories = postsCategories.reduce(
-          (prev: string[], curr: string[]) => prev.concat(curr),
-          []
-        )
-        setCategories(inputCategories)
-      }
     },
   })
 
@@ -56,12 +49,11 @@ const LibraryPage: NextPage = () => {
       <Container className={classes.container}>
         <section className={classes.section}>
           <h1 className={classes.title}>Biblioteca digital</h1>
-          {
-            <Filter
-              postCategories={categories}
-              refetch={(data) => refetch({ filters: data })}
-            />
-          }
+          <Filter
+            defaultCategory={defaultCategory}
+            postCategories={postsCategories}
+            refetch={(data) => refetch({ filters: data })}
+          />
           <Row>
             {!loading ? (
               posts.length ? (
@@ -82,6 +74,36 @@ const LibraryPage: NextPage = () => {
       </Container>
     </Layout>
   )
+}
+
+export const getServerSideProps = async (ctx: GetServerSidePropsContext) => {
+  const apolloClient = initializeApolloClient()
+  const { data } = await apolloClient.query({
+    query: GET_LISTS_OF_POSTS,
+    variables: { locale: ctx.locale },
+    context: { ms: microServices.strapi },
+  })
+
+  const categories = () => {
+    const postsCategories = data.posts.data.map((post: any) =>
+      post.attributes.postCategories.data.map(
+        (category: any) => category.attributes.category
+      )
+    )
+    return postsCategories.reduce(
+      (prev: string[], curr: string[]) => prev.concat(curr),
+      []
+    )
+  }
+
+  const defaultCategory = !ctx.query.category ? '' : ctx.query.category
+
+  return {
+    props: {
+      defaultCategory: defaultCategory,
+      postsCategories: categories(),
+    },
+  }
 }
 
 export default LibraryPage
