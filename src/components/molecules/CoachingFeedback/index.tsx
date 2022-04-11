c4657381840a3b045e6358166a25ddb72825b0aa
sindi <sindidaconceicao@gmail.com>
2022-04-11 16:43:40 -0400

    [feature][8b4d0c] function feedback

--- src/components/molecules/CoachingFeedback/index.tsx
+++ src/components/molecules/CoachingFeedback/index.tsx
@@ -1,36 +1,64 @@
-//next components
+//main tools
 import { useState } from 'react'
+import { useSession } from 'next-auth/react'
 
 // bootstrap components
+import { CheckSquare } from 'react-bootstrap-icons'
 import { Button, Col, Modal, Row } from 'react-bootstrap'
 
 // components
 import { Ratings } from 'components/atoms/Rating'
 
+// commoms
+import { microServices } from 'commons'
+
 // gql
-import COACHING_FEEDBACK from 'lib/mutations/Feedback/CoachingFeedback.gql'
+import { useMutation } from '@apollo/client'
+import COACH_FEEDBACK from 'lib/mutations/Feedback/coachFeedback.gql'
+import COACHEE_FEEDBACK from 'lib/mutations/Feedback/coacheeFeedback.gql'
 
 //styles
 import classes from 'styles/CoachingFeedback/coachingFeedback.module.scss'
 
 //types
 import { FC } from 'react'
-import { CheckSquare } from 'react-bootstrap-icons'
-import { useMutation } from '@apollo/client'
-import { microServices } from 'commons'
+import { FeedbackProps } from 'types/components/feedback'
 
-export const CoachingFeedback: FC = () => {
+export const CoachingFeedback: FC<{
+  feedbackId: number
+  coachingSessionId: number
+}> = ({ coachingSessionId, feedbackId }) => {
+  const { data } = useSession()
   const [showModal, setShowModal] = useState(true)
   const [showCheck, setShowCheck] = useState(false)
+  const [feedback, setFeedback] = useState<FeedbackProps[]>([])
 
-  const [coachingFeedback] = useMutation(COACHING_FEEDBACK, {
-    onCompleted: () => setShowCheck(true),
-    onError: (err) => console.log(err),
-    context: { ms: microServices.backend },
-  })
+  const [coachingFeedback] = useMutation(
+    data?.user.coachee ? COACHEE_FEEDBACK : COACH_FEEDBACK,
+    {
+      context: { ms: microServices.backend },
+      onCompleted: () => setShowCheck(true),
+    }
+  )
 
   const handleOpenCheck = () => {
-    coachingFeedback({ variables: {} })
+    if (data?.user.coachee) {
+      coachingFeedback({
+        variables: {
+          feedbackId: feedbackId,
+          coacheeFeedback: feedback,
+          coachingSessionId: coachingSessionId,
+        },
+      })
+    } else {
+      coachingFeedback({
+        variables: {
+          feedbackId: feedbackId,
+          coachFeedback: feedback,
+          coachingSessionId: coachingSessionId,
+        },
+      })
+    }
     setShowModal(false)
   }
 
@@ -49,7 +77,11 @@ export const CoachingFeedback: FC = () => {
             <Row className={classes.rating}>
               <h4>¿Que tal te fue en la sesión?</h4>
               <p>Podrías ayudarnos a calificar audio y video </p>
-              <Ratings />
+              <Ratings
+                feedback={feedback}
+                feedbackId={feedbackId}
+                setFeedback={setFeedback}
+              />
             </Row>
             <Col md={4} className={classes.body_button}>
               <Button className={classes.button} onClick={handleOpenCheck}>
