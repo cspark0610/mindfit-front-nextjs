f14f84a79f4a6e53fe120ad7b169bd016264984a
sindi <sindidaconceicao@gmail.com>
2022-04-15 10:38:39 -0400

    [feature][98bcd9] function graph satisfaction

--- src/components/organisms/DashboardOrg/Satisfaction/index.tsx
+++ src/components/organisms/DashboardOrg/Satisfaction/index.tsx
@@ -1,5 +1,5 @@
 // bootsrap components
-import { Col, ProgressBar, Row } from 'react-bootstrap'
+import { Col, ProgressBar, Row, Spinner } from 'react-bootstrap'
 
 // prime components
 import { Knob } from 'primereact/knob'
@@ -16,82 +16,65 @@ import { useQuery } from '@apollo/client'
 import { microServices } from 'commons'
 
 export const Satisfaction: FC<{ content: any }> = ({ content }) => {
-  const [data, setData] = useState()
+  const [sessionsSatisfaction, setSessionsSatisfaction] = useState([
+    { questionCodename: '', value: NaN },
+  ])
 
-  const { loading } = useQuery(SATISFACTION, {
+  const { loading, data } = useQuery(SATISFACTION, {
     context: { ms: microServices.backend },
     onCompleted: (data) => {
-      setData(data.getOrganizationCoacheesSatisfaction)
+      setSessionsSatisfaction(
+        data.getOrganizationCoacheesSatisfaction.sessionsSatisfaction
+      )
     },
   })
 
   return (
     <section className={`mb-5 ${classes.section}`}>
-      <h3 className={`mb-5 text-center ${classes.blue}`}>{content.title}</h3>
-      <Knob
-        max={10}
-        value={5.9}
-        readOnly
-        textColor='#1a7bee'
-        valueColor='#045095'
-        valueTemplate={'{value}/10'}
-        className={classes.knob}
-        strokeWidth={8}
-      />
-      <Col xs={6} className='m-auto text-center'>
-        <p className={`fw-bold ${classes.gray}`}>{content.description}</p>
-      </Col>
-      <Row xs='auto' className='justify-content-center align-items-end'>
-        <Col xs={8} className='mt-4'>
-          <p className={classes.gray}>
-            Mi entrenador y yo nos respetamos mutuamente
-          </p>
-          <ProgressBar className={classes.progressBar}>
-            <ProgressBar
-              max={6}
-              now={4.6}
-              className={classes.bar}
-              style={{ backgroundColor: '#1a7bee' }}
-            />
-          </ProgressBar>
-        </Col>
-        <Col>
-          <h3 className={`fw-bold ${classes.blue}`}>4.6</h3>
-        </Col>
-        <Col xs={8} className='mt-4'>
-          <p className={classes.gray}>
-            Estamos de acuerdo en lo que es importante que trabaje
-          </p>
-          <ProgressBar className={classes.progressBar}>
-            <ProgressBar
-              max={6}
-              now={5.2}
-              className={classes.bar}
-              style={{ backgroundColor: '#fe5000' }}
-            />
-          </ProgressBar>
-        </Col>
-        <Col>
-          <h3 className={`fw-bold ${classes.blue}`}>5.2</h3>
-        </Col>
-        <Col xs={8} className='mt-4'>
-          <p className={classes.gray}>
-            Siento que las cosas que hago en el coaching me ayudaran a lograr
-            los cambios que quiero
-          </p>
-          <ProgressBar className={classes.progressBar}>
-            <ProgressBar
-              max={6}
-              now={3.5}
-              className={classes.bar}
-              style={{ backgroundColor: '#2fb684' }}
-            />
-          </ProgressBar>
-        </Col>
-        <Col>
-          <h3 className={`fw-bold ${classes.blue}`}>3.5</h3>
-        </Col>
-      </Row>
+      {loading ? (
+        <Spinner animation='border' />
+      ) : !data.averageSatisfaction ? (
+        <p>No existen suficientes datos para mostrar esta gráfica</p>
+      ) : (
+        <>
+          <h3 className={`mb-5 text-center ${classes.blue}`}>{content.title}</h3>
+          <Knob
+            max={10}
+            value={5.9}
+            readOnly
+            textColor='#1a7bee'
+            valueColor='#045095'
+            valueTemplate={'{value}/10'}
+            className={classes.knob}
+            strokeWidth={8}
+          />
+          <Col xs={6} className='m-auto text-center'>
+            <p className={`fw-bold ${classes.gray}`}>
+              {content.Description}{' '}{data.averageSatisfaction}
+            </p>
+          </Col>
+          <Row xs='auto' className='justify-content-center align-items-end'>
+            {sessionsSatisfaction.map((item) => (
+              <>
+                <Col xs={8} className='mt-4'>
+                  <p className={classes.gray}>{item.questionCodename}</p>
+                  <ProgressBar className={classes.progressBar}>
+                    <ProgressBar
+                      max={6}
+                      now={item.value}
+                      className={classes.bar}
+                      style={{ backgroundColor: '#1a7bee' }}
+                    />
+                  </ProgressBar>
+                </Col>
+                <Col>
+                  <h3 className={`fw-bold ${classes.blue}`}>{item.value}</h3>
+                </Col>
+              </>
+            ))}
+          </Row>
+        </>
+      )}
     </section>
   )
 }
